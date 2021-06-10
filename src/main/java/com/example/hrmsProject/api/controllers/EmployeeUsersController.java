12a0bd05175dc71d8984a4ba076e56a3d452a347
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-06-10 10:52:33 +0300

    küçük düzeltme

--- src/main/java/com/example/hrmsProject/api/controllers/EmployeeUsersController.java
+++ src/main/java/com/example/hrmsProject/api/controllers/EmployeeUsersController.java
@@ -1,6 +1,9 @@
 package com.example.hrmsProject.api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.hrmsProject.business.abstracts.EmployeeUserService;
+import com.example.hrmsProject.core.utilities.results.DataResult;
 import com.example.hrmsProject.core.utilities.results.Result;
 import com.example.hrmsProject.entities.concretes.EmployeeUser;
 
@@ -25,10 +29,18 @@ public class EmployeeUsersController {
 	}
 
 
+	@GetMapping("/getall")
+	public DataResult<List<EmployeeUser>> getAll(){
+		return this.employeeUserService.getAll();
+	}
+	
 
 	@PostMapping("/register")
 	public Result register(@RequestBody EmployeeUser employeeUser, @RequestParam String repeatPassword) {
 		return this.employeeUserService.register(employeeUser, repeatPassword);
 	}
 }
+
+
+
 
